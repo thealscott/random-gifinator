fa6eafbf83a18f080f2e175487185754750faaad
Al Scott <alasdairs@criticalmass.com>
2014-08-21 16:27:24 +0100

    reworked the letter syncing functionality

--- list_files.php
+++ list_files.php
@@ -14,5 +14,21 @@ foreach($folders as $folder) {
     $file_lists[$folder] = $files;
 }
 
+// Get featured Letter gifs
+$folders = scandir('images/letters');
+$folders = array_diff($folders, $ignore);
+
+foreach($folders as $folder) {
+    // for each sub category we get the list of files within
+    $files = scandir('images/letters/'.$folder);
+    $files = array_values(array_diff($files, $ignore));
+    
+    $featured_file_lists[$folder] = $files;
+}
+
+
 // return the data as JSON
-print_r(json_encode($file_lists));
+print_r(json_encode(array(
+    'gifs' => $file_lists,
+    'letters' => $featured_file_lists
+)));
